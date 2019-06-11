915435077540e0b684ffe632481be1d9a935b174
feiwang <feiwang@x2era.com>
2019-06-11 16:08:18 +0800

    使用 kotlin 写adapter事件和点击事件

--- app/src/main/java/com/kuanquan/universalcomponents/adapter/KotlinAdapter.kt
+++ app/src/main/java/com/kuanquan/universalcomponents/adapter/KotlinAdapter.kt
@@ -1,5 +1,6 @@
 package com.kuanquan.universalcomponents.adapter
 
+import android.annotation.SuppressLint
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,8 @@ import com.kuanquan.universalcomponents.R
 
 class KotlinAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    var lists = ArrayList<String>()
+//    var lists = ArrayList<String>()
+private lateinit var lists: ArrayList<String>
 
     fun setData(lists: ArrayList<String>){
         this.lists = lists
@@ -17,24 +19,24 @@ class KotlinAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): RecyclerView.ViewHolder {
-        val rootView = LayoutInflater.from(parent.context).inflate(R.layout.item_layout_adapter, null, false)
+        val rootView = LayoutInflater.from(parent.context).inflate(R.layout.item_layout_adapter, parent, false)
         return MyHolder(rootView)
     }
 
     override fun getItemCount(): Int {
-//        return if(lists != null) lists.size else {0}
-        return lists?.size!!
+        return if(lists != null) lists.size else {0}
+//        return lists.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, @SuppressLint("RecyclerView") position: Int) {
         if (holder is MyHolder) {
             // 设置值
-            holder?.textView?.text = lists[position]
+            holder.textView?.text = lists[position]
 
             // 点击事件
             holder.itemView.setOnClickListener(object :View.OnClickListener{
                 override fun onClick(v: View?) {
-                    iKotlinItemClickListener!!.onItemClickListener(position)
+                    iKotlinItemClickListener?.onItemClickListener(position)
                 }
             })
         }
@@ -42,7 +44,7 @@ class KotlinAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         // !! 断言
-        var textView = itemView!!.findViewById<TextView>(R.id.text_view)
+        var textView = itemView.findViewById<TextView>(R.id.text_view)
     }
 
     // 对外提供自定义接口的set方法
