ffeafcbe5b672dba84813538702b779763ce1e96
feiwang <feiwang@x2era.com>
2019-06-12 11:11:46 +0800

    模块化

---
+++ mine/src/main/java/com/kuanquan/mine/MineActivity.kt
@@ -0,0 +1,15 @@
+package com.kuanquan.mine
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 首页模块的入口
+ */
+class MineActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mine)
+    }
+}
