91efcfa990c31c9d808558ad96598801620a2c90
feiwang <feiwang@x2era.com>
2019-06-12 18:26:34 +0800

    模块化

--- app/src/main/java/com/kuanquan/universalcomponents/HomeActivity.kt
+++ app/src/main/java/com/kuanquan/universalcomponents/HomeActivity.kt
@@ -1,13 +1,67 @@
 package com.kuanquan.universalcomponents
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
+import com.base.library.base.BaseViewModelActivity
+import com.base.library.utils.LogUtil
+import com.kuanquan.universalcomponents.fragment.*
+import com.kuanquan.universalcomponents.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseViewModelActivity<MainViewModel>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+    override fun getLayoutId(): Int {
+        return R.layout.activity_home
+    }
+
+    override fun initView() {
+        super.initView()
+    }
+
+    override fun initData() {
+        if (bottom_navigation_view != null) {
+            bottom_navigation_view.initView(this, R.id.main_frame,
+                HomeFragment(), ClassifyFragment(), StarFragment(), ShopCartFragment(), MineFragment()
+            )
+        } else {
+            LogUtil.e("View 为空")
+        }
+    }
+
+    override fun isBindEventBusHere(): Boolean {
+        return false
+    }
+
+    override fun dataObserver() {
+
+    }
+
+    override fun onClick(v: View?) {
+
+    }
+
+    override fun createViewModel(): MainViewModel {
+        return createViewModel(this,MainViewModel::class.java)
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onSaveInstanceState(outState: Bundle?) {
+//        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val savedIndex = savedInstanceState?.getInt("index")
+        if (savedIndex != bottom_navigation_view.index) {
+            if (bottom_navigation_view.fragments[0].isAdded) {
+                supportFragmentManager.beginTransaction().hide(bottom_navigation_view.fragments[0]).commit()
+            }
+            when (savedIndex) {
+                1 -> bottom_navigation_view.disPlay(1)
+                2 -> bottom_navigation_view.disPlay(2)
+                3 -> bottom_navigation_view.disPlay(3)
+            }
+        }
     }
 }
