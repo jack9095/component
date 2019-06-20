553da014e53e9fcd30fde74feea5fb7fa5381771
feiwang <feiwang@x2era.com>
2019-06-20 14:20:58 +0800

    RecyclerView的分割线

---
+++ app/src/main/java/com/kuanquan/universalcomponents/adapter/ClassifyAdapter.kt
@@ -0,0 +1,50 @@
+package com.kuanquan.universalcomponents.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.base.library.utils.CollectionsUtil
+import com.base.library.utils.glide.invocation.ImageLoaderManager
+import com.kuanquan.universalcomponents.R
+import com.kuanquan.universalcomponents.kotlinTest.UserBean
+
+class ClassifyAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var lists: List<UserBean>? = null
+    var context: Context? = null
+
+    fun setData(lists: List<UserBean>){
+        this.lists = lists
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): RecyclerView.ViewHolder {
+        context = parent.context
+        val viewItem = LayoutInflater.from(parent.context).inflate(R.layout.classify_item_layout, parent, false)
+        return ClassifyitemAdapter(viewItem)
+    }
+
+    override fun getItemCount(): Int {
+        return lists?.size ?: 0
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val bean = lists?.get(position)
+        if (bean == null) {
+
+        }
+        if (holder is ClassifyitemAdapter) {
+//            CollectionsUtil.setTextView(holder.tv_classify_item,bean?.name)
+            ImageLoaderManager.getInstance().displayImageNetUrl(context,bean?.sex,R.mipmap.ic_launcher,holder.im_classify_item)
+        }
+    }
+
+    class ClassifyitemAdapter(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var im_classify_item = itemView.findViewById<ImageView>(R.id.im_classify_item)
+        var tv_classify_item = itemView.findViewById<TextView>(R.id.tv_classify_item)
+    }
+}
--- app/src/main/java/com/kuanquan/universalcomponents/fragment/ClassifyFragment.kt
+++ app/src/main/java/com/kuanquan/universalcomponents/fragment/ClassifyFragment.kt
@@ -1,10 +1,15 @@
 package com.kuanquan.universalcomponents.fragment
 
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.base.library.base.BaseViewModelFragment
+import com.base.library.widget.RecycleGridDivider
 import com.kuanquan.universalcomponents.R
+import com.kuanquan.universalcomponents.adapter.ClassifyAdapter
+import com.kuanquan.universalcomponents.kotlinTest.UserBean
 import com.kuanquan.universalcomponents.viewmodel.ClassifyViewModel
 import com.scwang.smartrefresh.layout.SmartRefreshLayout
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener
@@ -14,6 +19,8 @@ class ClassifyFragment : BaseViewModelFragment<ClassifyViewModel>() {
 
     var recyclerView: RecyclerView? = null
     var refreshLayout: SmartRefreshLayout? = null
+    var classifyAdapter: ClassifyAdapter? = null
+    var lists = ArrayList<UserBean>()
 
     override fun onClick(v: View?) {
     }
@@ -28,22 +35,34 @@ class ClassifyFragment : BaseViewModelFragment<ClassifyViewModel>() {
 
     override fun initView() {
         super.initView()
-        top_navigation_a_f.setTvTitle("分类")
-        top_navigation_a_f.setHintLeftTextView(true)
+        mTopNavigationLayout = view.findViewById(R.id.top_navigation_a_f)
+        mTopNavigationLayout.setTvTitle("分类")
+        mTopNavigationLayout.setHintLeftTextView(true)
 
-        recyclerView = view.findViewById(R.id.common_recycler_view)
-        refreshLayout = view.findViewById(R.id.common_refresh_Layout)
+        recyclerView = view.findViewById(R.id.common_recycler_view_classify)
+        recyclerView?.layoutManager = GridLayoutManager(getActivity(),2)
+//        recyclerView?.addItemDecoration(DividerItemDecoration(getActivity(),DividerItemDecoration.HORIZONTAL))
+        recyclerView?.addItemDecoration(RecycleGridDivider(10))
+//        recyclerView?.addItemDecoration(GridDividerItemDecoration(getContext(), 10,true))
+        classifyAdapter = ClassifyAdapter()
+        recyclerView?.adapter = classifyAdapter
+
+        refreshLayout = view.findViewById(R.id.common_refresh_Layout_classify)
 
         refreshLayout?.setOnRefreshListener { refreshlayout ->
-            refreshlayout.finishRefresh(1000/*,false*/) //传入false表示刷新失败
+            refreshlayout.finishRefresh(1000/*,false*/) // 传入false表示刷新失败
         }
 
         refreshLayout?.setOnLoadMoreListener { refreshlayout ->
-            refreshlayout.finishLoadMore(500/*,false*/)//传入false表示加载失败
+            refreshlayout.finishLoadMore(500/*,false*/) // 传入false表示加载失败
         }
     }
 
     override fun initData(savedInstanceState: Bundle?) {
+        for (i in 1..90){
+            lists.add(UserBean())
+        }
+        classifyAdapter?.setData(lists)
     }
 
     override fun isBindEventBusHere(): Boolean {
