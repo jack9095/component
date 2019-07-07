3bb87a5dd950bc010b7dc34cbefd3d34d3edf6d6
weall <406981543@qq.com>
2019-07-07 21:16:16 +0800

    我的address

---
+++ mine/src/main/java/com/kuanquan/mine/AboutUsActivity.kt
@@ -0,0 +1,15 @@
+package com.kuanquan.mine
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 关于我们
+ */
+class AboutUsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+    }
+}
