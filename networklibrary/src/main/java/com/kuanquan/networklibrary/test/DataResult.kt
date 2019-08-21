21cd9f849e36036f4d200149305a1a31875a2829
feiwang <feiwang@x2era.com>
2019-08-21 18:40:28 +0800

    协程结合retrofit进行网络请求

--- networklibrary/src/main/java/com/kuanquan/networklibrary/test/DataResult.kt
+++ networklibrary/src/main/java/com/kuanquan/networklibrary/test/DataResult.kt
@@ -1,18 +1,10 @@
 package com.kuanquan.networklibrary.test
 
 data class DataModel(
-    val _id: String,
-    val createdAt: String,
-    val desc: String,
-    val publishedAt: String,
-    val source: String,
     val type: String,
-    val url: String,
-    val used: Boolean,
-    val who: String
+    val url: String
 )
 
 data class DataResult(
-    val error: Boolean,
     val results: List<DataModel>
 )
